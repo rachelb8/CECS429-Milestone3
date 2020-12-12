e627a6dcdac4093335c9bed5fed8961e924c8a94
danielmendez7899 <44877100+danielmendez7899@users.noreply.github.com>
2020-12-12 15:57:53 -0800

    Added Rocchio.java class

---
+++ src/main/java/cecs429/classification/Rocchio.java
@@ -0,0 +1,14 @@
+package cecs429.classification;
+
+public class Rocchio {
+
+    public Rocchio(VectorSpace vs) {
+
+    }
+
+    // public VectorSpace rocchioClassify() {
+    //     VectorSpace result;
+    //     return result;
+    // }
+    
+}
